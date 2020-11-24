d97a670fb9090638c6b13b0867daf8445eb2e904
Boris Gordon <bg@38th.ru>
2020-11-24 20:27:44 +0300

    33
    Signed-off-by: Boris Gordon <bg@38th.ru>

--- StopEXE.ps1
+++ StopEXE.ps1
@@ -3,19 +3,17 @@ clear
 $ConstPath = "C:\Program Files (x86)\Minecraft Launcher\MinecraftLauncher.exe"
 
 $IDs = @()
-$NameS = @()
 $PathS = @($ConstPath)
 
 foreach ($mainItem in Get-Process -Name *Minecraft* )
 {
 	$IDs += $mainItem.Id
 	$ProcName = $mainItem.MainModule.ModuleName
-	if (-not ($ProcName -in $NameS))
+	$cmdLine = Get-WmiObject Win32_Process -Filter "name = '$ProcName'" | Select-Object CommandLine
+	$Name = $cmdLine[0].CommandLine.Replace('"', '')
+	if (-not ($Name -in $PathS))
 	{
-		$NameS += $ProcName
-		$cmdLine = Get-WmiObject Win32_Process -Filter "name = '$ProcName'" | Select-Object CommandLine
-		$PathS += $cmdLine[0].CommandLine.Replace('"', '')
-	
+		$PathS += $Name
 	}
 }
 $IDs_slave = @()
@@ -37,18 +35,25 @@ foreach ($item in $IDs)
 foreach ($FullPath in $PathS)
 {
 	$FilePath = Split-Path $FullPath
-	#$FileName = Split-Path $FullPath -Leaf
-
-	$rndName = -join (((48..57) + (65..90) + (97..122)) * 80 | Get-Random -Count 16 | % { [char]$_ }) + ".txt"
-	$ConfFilePath = $FilePath + '\check.cnf'
-	$NewFileName = $FilePath + '\' + $rndName
-	if (-not (Test-Path $ConfFilePath))
+	if (Test-Path $FullPath)
 	{
-		New-Item -Path $ConfFilePath -ItemType File
+		#$FileName = Split-Path $FullPath -Leaf
+
+		$rndName = -join (((48..57) + (65..90) + (97..122)) * 80 | Get-Random -Count 16 | % { [char]$_ }) + ".txt"
+		$ConfFilePath = $FilePath + '\check.cnf'
+		#$NewFileName = $FilePath + '\' + $rndName
+		if (-not (Test-Path $ConfFilePath))
+		{
+			New-Item -Path $ConfFilePath -ItemType File
+		}
+		Set-Content $ConfFilePath $rndName -Force
+		Rename-Item -Path $FullPath -NewName $rndName
+		Write-Host "Move: " + $FullPath
 	}
-	Set-Content $ConfFilePath $rndName -Force
-	Rename-Item -Path $FullPath -NewName $rndName
-	
+	else 
+	{
+		Write-Host $FullPath + " не найден"
+	}
 }
 
 # { (Get-Process -Name *Minecraft* -id $_.ParentProcessId -ea Ignore) -eq $null } | Write-Host 
--- UnLock.ps1
+++ UnLock.ps1
@@ -1,2 +1,15 @@
 $ConstPath = "C:\Program Files (x86)\Minecraft Launcher\MinecraftLauncher.exe"
 
+$FilePath = Split-Path $ConstPath
+$FileName = Split-Path $ConstPath -Leaf
+$ConfFilePath = $FilePath + '\check.cnf'
+if (Test-Path $ConfFilePath)
+{
+	$Conf = Get-Content -Path $ConfFilePath
+	$SourceFileName = if ($Conf -is [array]) { $Conf[0] }Else { $Conf }
+	$SourcePath = $FilePath + '\' + $SourceFileName
+	if (Test-Path $SourcePath)
+	{
+		Rename-Item -Path $SourcePath -NewName $FileName
+	}	
+}
