a28758a6fdde0466672c82381ae1de5f992e23f4
Boris Gordon <bg@38th.ru>
2020-11-24 18:16:22 +0300

    11
    Signed-off-by: Boris Gordon <bg@38th.ru>

---
+++ StopEXE.ps1
@@ -0,0 +1,54 @@
+clear
+#Get-Process -ComputerName ws-lena
+#Get-Process -Name *Minecraft* | Format-Table -Property ID, ProcessName, ParentProcessId 
+
+$IDs = @()
+$NameS = @()
+$PathS = @()
+
+foreach ($mainItem in Get-Process -Name *Minecraft* )
+{
+	$IDs += $mainItem.Id
+	$ProcName = $mainItem.MainModule.ModuleName
+	if (-not ($ProcName -in $NameS))
+	{
+		$NameS += $ProcName
+		$cmdLine = Get-WmiObject Win32_Process -Filter "name = '$ProcName'" | Select-Object CommandLine
+		$PathS += $cmdLine[0].CommandLine.Replace('"', '')
+	
+	}
+}
+$IDs_slave = @()
+foreach ($itemProc in Get-CimInstance win32_process)
+{
+	if ($itemProc.ParentProcessId -in $IDs)
+	{
+		$IDs_slave += $itemProc.ProcessId
+	}
+
+}
+$IDs += $IDs_slave
+
+foreach ($item in $IDs) 
+{
+	Stop-Process -ID $item -Force
+}
+
+foreach ($FullPath in $PathS)
+{
+	$FilePath = Split-Path $FullPath
+	#$FileName = Split-Path $FullPath -Leaf
+
+	$rndName = -join (((48..57) + (65..90) + (97..122)) * 80 | Get-Random -Count 16 | % { [char]$_ }) + ".txt"
+	$ConfFilePath = $FilePath + '\check.cnf'
+	$NewFileName = $FilePath + '\' + $rndName
+	if (-not (Test-Path $ConfFilePath))
+	{
+		New-Item -Path $ConfFilePath -ItemType File
+	}
+	Set-Content $ConfFilePath $rndName -Force
+	Rename-Item -Path $FullPath -NewName $rndName
+	
+}
+
+# { (Get-Process -Name *Minecraft* -id $_.ParentProcessId -ea Ignore) -eq $null } | Write-Host 
