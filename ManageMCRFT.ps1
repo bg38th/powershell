b4b4c8990d3b2d7b19323a7e08fbeac15d1501f0
Boris Gordon <bg@38th.ru>
2020-12-30 21:57:51 +0300

    Time to class

--- ManageMCRFT.ps1
+++ ManageMCRFT.ps1
@@ -1,102 +1,7 @@
+Using module  .\TimeInterval.psm1
+
 Clear-Host
 #$ConstPath = "C:\Program Files (x86)\Minecraft Launcher\MinecraftLauncher.exe"
-class TimeIntervalConfig
-{
-	$Config
-
-	hidden [string]$DefaultIntervalJSON = '[
-   {num: 1, start: "00:00", end: "06:45"},
-   {num: 2, start: "00:15", end: "14:10"},
-   {num: 3, start: "19:00", end: "21:00"},
-   {num: 4, start: "22:00", end: "24:00"}
-]'
-	hidden [string]$constConfigJSON = '[
-   {day: 0, times: ["default_3"]},
-   {day: 1, times: ["default_2", "default_3"]},
-   {day: 2, times: [{start: "00:00", end: "06:45"},"default_3"]},
-   {day: 3, times: ["default_2", "default_3"]},
-   {day: 4, times: ["default_2", "default_3"]},
-   {day: 5, times: ["default_2"]}
-]'
-
-	<#
- 	[string] GetDefaultRegistryJSON([string]$sCurConf)
-	{
-		[string]$TimeConfigPath = $sCurConf + + "\TIME"
-		[string]$DefaultJSON = Get-ItemProperty -Path $TimeConfigPath -name DefaultInterval -ErrorAction silentlycontinue
-		if ( $null -eq $DefaultJSON)
-		{ $DefaultJSON = New-ItemProperty -Path $TimeConfigPath -name DefaultInterval -Value $this.DefaultIntervalJSON }
-	
-		return $DefaultJSON.DefaultInterval
-	}
-	
-	[string] GetConfigRegistryJSON([string]$sCurConf)
-	{
-		[string]$TimeConfigPath = $sCurConf + + "\TIME"
-		[string]$ConfigJSON = Get-ItemProperty -Path $TimeConfigPath -name Config -ErrorAction silentlycontinue
-		if ( $null -eq $ConfigJSON)
-		{ $ConfigJSON = New-ItemProperty -Path $TimeConfigPath -name Config -Value $this.constConfigJSON }
-	
-		return $ConfigJSON.Config
-	}
- #>
-	TimeIntervalConfig([string]$sCurConf)
-	{
-		function GetRegistryJSON([string]$sCurConf, [string]$Var, [string]$Default)
-		{
-			[string]$TimePath = $sCurConf + "\TIME"
-			$VarJSON = Get-ItemProperty -Path $TimePath -name $Var -ErrorAction silentlycontinue
-			if ( $null -eq $VarJSON -or $VarJSON -eq "")
-			{ $VarJSON = New-ItemProperty -Path $TimePath -name $Var -PropertyType MultiString -Value $Default }
-		
-			[string]$sRet = $VarJSON | Get-ItemPropertyValue -name $Var;
-			return $sRet
-		}
-		
-		$TimeDefault = GetRegistryJSON $sCurConf "DefaultInterval" $this.DefaultIntervalJSON  | ConvertFrom-Json;
-		$this.Config = GetRegistryJSON $sCurConf "Config" $this.constConfigJSON | ConvertFrom-Json;
-		for ($i = 0; $i -lt $this.Config.length ; $i++)
-		{
-			for ($j = 0; $j -lt $this.Config[$i].times.length ; $j++ )
-			{
-				If ($this.Config[$i].times[$j].GetType().Name -eq "String" -and $this.Config[$i].times[$j] -like "default_*")
-				{
-					$numStr = $this.Config[$i].times[$j] -replace "default_", ""
-					$num = [convert]::ToInt32($numStr, 10)
-					$curDefaultInterval = $TimeDefault.where( { $_.num -eq $num })
-					# $this.Config[$i].times[$j] = @{start = $curDefaultInterval.start; end = $curDefaultInterval.end };
-					$curTime = '{start: ' + $curDefaultInterval.start + ', end: ' + $curDefaultInterval.end + '}';
-					$this.Config[$i].times[$j] = $curTime | ConvertFrom-Json;
-				}
-			}
-		}
-	}
-}
-
-function CheckWorkTime($timeConfig)
-{
-	$curDay = Get-Date #-Date "16.12.2020 19:01"
-	$curTime = Get-Date -Date $curDay -Format t
-
-	$curConfItem = GetCurConfItem $curDay $timeConfig
-	if ($curConfItem.Count -ne 0)
-	{
-		$curInterval = $curConfItem.where( { ($curTime -ge $_.start) -and ($curTime -le $_.end) })
-		if ($curInterval.Count -ne 0)
-		{
-			return $true
-		}
-	}
-
-	return $false
-}
-
-function GetCurConfItem($curDay, $timeConfig)
-{
-	$curConfItem = $constConfig.where( { $_.day -eq $curDay.DayOfWeek })
-	return $curConfItem.times
-}
-
 function GetScriptConf()
 {
 	if ( (Test-Path -Path Registry::HKEY_CURRENT_USER\Software\BGSoft) )
@@ -195,7 +100,7 @@ function GetProcessConf([string]$rScriptConf, [string]$sProcessPath)
 
 function SetCriptFileName([string]$rProcessConf)
 {
-	$sNewFileName = -join (((48..57) + (65..90) + (97..122)) * 80 | Get-Random -Count 16 | % { [char]$_ }) + ".txt"
+	$sNewFileName = -join (((48..57) + (65..90) + (97..122)) * 80 | Get-Random -Count 16 | ForEach-Object { [char]$_ }) + ".txt"
 	Set-ItemProperty -Path $rProcessConf -Name "newFileName" -Value $sNewFileName
 
 	return $sNewFileName
@@ -211,10 +116,8 @@ function GetScriptFileName([string]$rProcessConf)
 $rScriptConf = GetScriptConf
 $sCurMask = GetMask $rScriptConf
 
-$oIntervalConfig = [TimeIntervalConfig]::new($rScriptConf);
-$intervalConfig = $oIntervalConfig.Config;
-
-$retBool = CheckWorkTime $intervalConfig 
+$oIntervalConfig = [TimeInterval]::new($rScriptConf);
+$retBool = $oIntervalConfig.CheckWorkTime($null);
 
 if ($retBool -and -not (CheckDoHomework $rScriptConf))
 {
@@ -253,7 +156,7 @@ else
 		$sCriptFileName = GetScriptFileName $rChildPart
 		$SourcePath = $FilePath + '\' + $sCriptFileName
 		$FileName = Get-ItemProperty -Path $rChildPart -Name "fileName" 
-		$res = Rename-Item -Path $SourcePath -NewName $FileName.fileName
+		Rename-Item -Path $SourcePath -NewName $FileName.fileName
 
 		Remove-Item $rChildPart
 
---
+++ TimeInterval.psm1
@@ -0,0 +1,90 @@
+@{
+	ModuleVersion          = "1.0.0.0"
+	Author                 = "<oris Gordon"
+	Copyright              = "38th.ru"
+	PowerShellVersion      = "7.0"
+	DotNetFrameworkVersion = "4.0"
+	FunctionsToExport      = "CheckWorkTime"
+}
+
+class TimeInterval
+{
+	$Config
+
+	hidden [string]$DefaultIntervalJSON = '[
+   {num: 1, start: "00:00", end: "06:45"},
+   {num: 2, start: "08:15", end: "14:10"},
+   {num: 3, start: "19:00", end: "21:00"},
+   {num: 4, start: "21:30", end: "23:59:59.9999999"},
+   {num: 5, start: "22:30", end: "23:59:59.9999999"}
+   ]'
+	hidden [string]$constConfigJSON = '[
+   {day: 0, times: ["default_1", "default_3", "default_4"]},
+   {day: 1, times: ["default_1", "default_2", "default_3", "default_4"]},
+   {day: 2, times: ["default_1", {start: "08:15", end: "14:10"},"default_3", "default_4"]},
+   {day: 3, times: ["default_1", "default_2", "default_3", "default_4"]},
+   {day: 4, times: ["default_1", "default_2", "default_3", "default_4"]},
+   {day: 5, times: ["default_1", "default_2", "default_5"]},
+   {day: 6, times: ["default_1", "default_5"]}
+]'
+
+	[bool]CheckWorkTime($curDay)
+	{
+		function GetCurConfItem($curDay, $timeConfig)
+		{
+			$curConfItem = $timeConfig.where( { $_.day -eq $curDay.DayOfWeek })
+			return $curConfItem.times
+		}
+		
+		if ( $null -eq $curDay )
+		{
+			$curDay = Get-Date #-Date "16.12.2020 19:01"
+		}
+
+		$curTime = Get-Date -Date $curDay -Format t
+
+		$curConfItem = GetCurConfItem $curDay $this.Config
+		if ($curConfItem.Count -ne 0)
+		{
+			$curInterval = $curConfItem.where( { ([datetime]$curTime -ge [datetime]$_.start) -and ([datetime]$curTime -le [datetime]$_.end) })
+			if ($curInterval.Count -ne 0)
+			{
+				return $true
+			}
+		}
+
+		return $false
+	}
+	
+	TimeInterval([string]$sCurConf)
+	{
+		function GetRegistryJSON([string]$sCurConf, [string]$Var, [string]$Default)
+		{
+			[string]$TimePath = $sCurConf + "\TIME"
+			$VarJSON = Get-ItemProperty -Path $TimePath -name $Var -ErrorAction silentlycontinue
+			if ( $null -eq $VarJSON -or $VarJSON -eq "")
+			{ $VarJSON = New-ItemProperty -Path $TimePath -name $Var -PropertyType MultiString -Value $Default }
+		
+			[string]$sRet = $VarJSON | Get-ItemPropertyValue -name $Var;
+			return $sRet
+		}
+		
+		$TimeDefault = GetRegistryJSON $sCurConf "DefaultInterval" $this.DefaultIntervalJSON  | ConvertFrom-Json;
+		$this.Config = GetRegistryJSON $sCurConf "Config" $this.constConfigJSON | ConvertFrom-Json;
+		for ($i = 0; $i -lt $this.Config.length ; $i++)
+		{
+			for ($j = 0; $j -lt $this.Config[$i].times.length ; $j++ )
+			{
+				If ($this.Config[$i].times[$j].GetType().Name -eq "String" -and $this.Config[$i].times[$j] -like "default_*")
+				{
+					$numStr = $this.Config[$i].times[$j] -replace "default_", ""
+					$num = [convert]::ToInt32($numStr, 10)
+					$curDefaultInterval = $TimeDefault.where( { $_.num -eq $num })
+					# $this.Config[$i].times[$j] = @{start = $curDefaultInterval.start; end = $curDefaultInterval.end };
+					$this.Config[$i].times[$j] = '{start: "' + $curDefaultInterval.start + '", end: "' + $curDefaultInterval.end + '"}' | ConvertFrom-Json;
+				}
+			}
+		}
+	}
+}
+   
