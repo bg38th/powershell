84c778727ab49e24e1756400ca5186ae565a12e8
Boris Gordon <bg@38th.ru>
2020-12-30 22:05:33 +0300

    Manage Mikrotik

--- ManageMCRFT.ps1
+++ ManageMCRFT.ps1
@@ -5,9 +5,18 @@ Clear-Host
 function GetScriptConf()
 {
 	if ( (Test-Path -Path Registry::HKEY_CURRENT_USER\Software\BGSoft) )
-	{ $rScriptConf = Get-Item -Path Registry::HKEY_CURRENT_USER\Software\BGSoft }
+	{ 
+		$rScriptConf = Get-Item -Path Registry::HKEY_CURRENT_USER\Software\BGSoft 
+	}
 	else 
-	{ $rScriptConf = New-Item -Path Registry::HKEY_CURRENT_USER\Software\BGSoft }
+	{ 
+		$rScriptConf = New-Item -Path Registry::HKEY_CURRENT_USER\Software\BGSoft 
+	}
+
+	if ( -not (Test-Path -Path Registry::HKEY_CURRENT_USER\Software\BGSoft\TIME)) 
+	{ 
+		New-Item -Path Registry::HKEY_CURRENT_USER\Software\BGSoft\TIME 
+	}
 
 	return "Registry::" + $rScriptConf 
 
---
+++ ManageMikrotik.ps1
@@ -0,0 +1,23 @@
+Using module  .\TimeInterval.psm1
+
+Clear-Host
+#$ConstPath = "C:\Program Files (x86)\Minecraft Launcher\MinecraftLauncher.exe"
+function GetScriptConf()
+{
+	if ( (Test-Path -Path Registry::HKEY_CURRENT_USER\Software\BGSoft) )
+	{ 
+		$rScriptConf = Get-Item -Path Registry::HKEY_CURRENT_USER\Software\BGSoft 
+	}
+	else 
+	{ 
+		$rScriptConf = New-Item -Path Registry::HKEY_CURRENT_USER\Software\BGSoft 
+	}
+
+	if ( -not (Test-Path -Path Registry::HKEY_CURRENT_USER\Software\BGSoft\TIME)) 
+	{ 
+		New-Item -Path Registry::HKEY_CURRENT_USER\Software\BGSoft\TIME 
+	}
+
+	return "Registry::" + $rScriptConf 
+
+}
